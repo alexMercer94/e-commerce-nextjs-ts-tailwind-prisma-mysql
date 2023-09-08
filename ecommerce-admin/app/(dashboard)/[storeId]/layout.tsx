baf094139c29ae5210343c899f9bf1ddd184ed2c
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2023-09-08 15:08:51 -0600

    Added Dashboard setup

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,28 @@
+import prismadb from '@/lib/prismadb';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { ReactNode } from 'react';
+
+const DashboardLayout = async ({ children, params }: { children: ReactNode; params: { storeId: string } }) => {
+    const { userId } = auth();
+
+    if (!userId) redirect('/sign-in');
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+            userId,
+        },
+    });
+
+    if (!store) redirect('/');
+
+    return (
+        <>
+            <div>Navbar</div>
+            {children}
+        </>
+    );
+};
+
+export default DashboardLayout;
