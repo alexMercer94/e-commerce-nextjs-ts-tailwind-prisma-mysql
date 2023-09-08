be7cc0ffabdf5647cb0b12cffb05ac960425f787
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2023-09-08 13:33:52 -0600

    Setup prima and added fisrt route to create stores

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,4 +1,5 @@
 import ModalProvider from '@/providers/modal-provider';
+import { ToasterProvider } from '@/providers/toast-provider';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ClerkProvider>
             <html lang="en">
                 <body className={inter.className}>
+                    <ToasterProvider />
                     <ModalProvider />
                     {children}
                 </body>
--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -6,7 +6,10 @@ import { Input } from '@/components/ui/input';
 import Modal from '@/components/ui/modal';
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 import * as z from 'zod';
 
 const formSchema = z.object({
@@ -15,6 +18,8 @@ const formSchema = z.object({
 
 const StoreModal = () => {
     const { isOpen, onClose } = useStoreModal();
+    const [loading, setLoading] = useState(false);
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -23,9 +28,15 @@ const StoreModal = () => {
     });
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-
-        // TODO: create store
+        try {
+            setLoading(true);
+            const response = await axios.post('/api/stores', values);
+            console.log(response.data);
+            toast.success('Store created.');
+        } catch (error) {
+            toast.error('Something went wrong.');
+            setLoading(false);
+        }
     };
 
     return (
@@ -46,17 +57,19 @@ const StoreModal = () => {
                                     <FormItem>
                                         <FormLabel>Name</FormLabel>
                                         <FormControl>
-                                            <Input placeholder="E-commerce" {...field} />
+                                            <Input disabled={loading} placeholder="E-commerce" {...field} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
                                 )}
                             />
                             <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                                <Button variant={'outline'} onClick={onClose}>
+                                <Button disabled={loading} variant={'outline'} onClick={onClose}>
                                     Cancel
                                 </Button>
-                                <Button type="submit">Continue</Button>
+                                <Button disabled={loading} type="submit">
+                                    Continue
+                                </Button>
                             </div>
                         </form>
                     </Form>
